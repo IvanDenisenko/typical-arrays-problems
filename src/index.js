714960aa69e8e1b4449917b5d6f905427b39df7d
Ivan Denisenko <57327265+IvanDenisenko@users.noreply.github.com>
2021-03-13 09:57:36 +0300

    fix: add underfined condition

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 
 exports.min = function min (array) {
-   if (array.length == 0) return 0;
-    let minimal = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (minimal > array[i]) minimal = array[i];
-    }
-    return minimal;
+   if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+   let minimal = array[0];
+   for (let i = 0; i < array.length; i++) {
+       if (minimal > array[i]) minimal = array[i];
+   }
+   return minimal;
 }
 
 exports.max = function max (array) {
-  if (array.length == 0) return 0;
-    let maxi = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (maxi < array[i]) maxi = array[i];
-    }
-    return maxi;
+   if (array === undefined) return 0;
+ if (array.length === 0) return 0;
+   let maxi = array[0];
+   for (let i = 0; i < array.length; i++) {
+       if (maxi < array[i]) maxi = array[i];
+   }
+   return maxi;
 }
 
 exports.avg = function avg (array) {
-  let count = 0;
-    if (array.length == 0) return 0;
-    for (let i = 0; i < array.length; i++) {
-       count += array[i]; 
-    }
-    return count/array.length;
+   if (array === undefined) return 0;
+ let count = 0;
+   if (array.length === 0) return 0;
+   for (let i = 0; i < array.length; i++) {
+      count += array[i]; 
+   }
+   return count/array.length;
 }
